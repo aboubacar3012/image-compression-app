2b11c2cb0d15b39929ef32ace831781c8e8757de
DIALLO ABOUBACAR <a.diallo3012@gmail.com>
2023-10-04 15:04:32 +0200

    image compression

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import imageCompression from "browser-image-compression";
+import JSZip from "jszip";
 
 function App() {
+  const [compressedImages, setCompressedImages] = useState([]);
+
+  const handleImageUpload = async (event) => {
+    const files = event.target.files;
+    const compressedImagesArray = [];
+
+    for (let i = 0; i < files.length; i++) {
+      const imageFile = files[i];
+      try {
+        const options = {
+          maxSizeMB: 0.3,
+          maxWidthOrHeight: 1024,
+          useWebWorker: true,
+        };
+        const compressedFile = await imageCompression(imageFile, options);
+        compressedImagesArray.push(compressedFile);
+      } catch (error) {
+        console.error("Error compressing image: ", error);
+      }
+    }
+
+    // @ts-ignore
+    setCompressedImages(compressedImagesArray);
+  };
+
+  const handleDownload = () => {
+    const zip = new JSZip();
+
+    compressedImages.forEach((image, index) => {
+      zip.file(`image_${index + 1}.jpg`, image);
+    });
+
+    zip.generateAsync({ type: "blob" }).then((blob) => {
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(blob);
+      link.download = "compressed_images.zip";
+      link.click();
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <h1>Nojyk Image Compression and ZIP</h1>
+      <input
+        type="file"
+        accept="image/*"
+        multiple
+        onChange={handleImageUpload}
+      />
+      {compressedImages.length > 0 && (
+        <button onClick={handleDownload}>Download ZIP</button>
+      )}
     </div>
   );
 }
